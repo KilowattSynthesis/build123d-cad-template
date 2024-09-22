43cef2ffc54a2410fa2fad192f4daaae7ea20efe
KilowattSynthesis <180148956+KilowattSynthesis@users.noreply.github.com>
2024-09-21 23:00:25 -0600

    Update template for ease of multiple models

--- cad/part1.py
+++ cad/part1.py
@@ -3,7 +3,30 @@ from pathlib import Path
 
 import build123d as bd
 
+from loguru import logger
+
+
+if os.getenv("CI"):
+
+    def show(*args: object) -> bd.Part:
+        """Do nothing (dummy function) to skip showing the CAD model in CI."""
+        logger.info(f"Skipping show({args}) in CI")
+        return args[0]
+else:
+    import ocp_vscode
+
+    def show(*args: object) -> bd.Part:
+        """Show the CAD model in the CAD viewer."""
+        ocp_vscode.show(*args)
+        return args[0]
+
+
 # Constants
+
+
+def validate():
+    """Raise if variables are not valid."""
+    pass
 
 
 def make_part1():
@@ -11,18 +34,22 @@ def make_part1():
     with bd.BuildPart() as part1:
         bd.Cylinder(radius=20, height=20)
 
-    return part1
+    return part1.part
 
 
 if __name__ == "__main__":
-    part1 = make_part1()
+    validate()
 
-    if not os.getenv("CI"):
-        from ocp_vscode import show
+    parts = {
+        "part1": make_part1(),
+    }
 
-        print("Showing CAD model(s)")
-        show(part1)
+    logger.info("Showing CAD model(s)")
 
     (export_folder := Path(__file__).parent.with_name("build")).mkdir(exist_ok=True)
-    bd.export_stl(part1.part, str(export_folder / "part1.stl"))
-    bd.export_step(part1.part, str(export_folder / "part1.step"))
+    for name, part in parts.items():
+        assert isinstance(part, bd.Part), f"{name} is not a Part"
+        # assert part.is_manifold is True, f"{name} is not manifold"
+
+        bd.export_stl(part, str(export_folder / f"{name}.stl"))
+        bd.export_step(part, str(export_folder / f"{name}.step"))
